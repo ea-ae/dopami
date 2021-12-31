f6f51b3a31c77964a2e51a131c2b6a62641f6763
ae <90932525+ea-ae@users.noreply.github.com>
2021-12-31 12:34:34 +0200

    Format existing code

--- dopami/Client/src/app.tsx
+++ dopami/Client/src/app.tsx
@@ -1,7 +1,15 @@
-import React from 'react'
+import { title } from 'process';
+import React, { useState } from 'react'
 
 import './index.css'
 
+
+const ListNumber = ({num}: {num: number}) => {
+    const [state, setState] = useState(num);
+    return (
+        <li className="select-none" onClick={() => setState(state + 1)}>Number {state}</li>
+    );
+}
 
 interface Items {
     title: string;
@@ -9,11 +17,12 @@ interface Items {
 }
 
 const App = (props: Items) => {
+    let {title, items} = props;
     return (
         <>
-        <div className="w-fit px-4 py-2 rounded-sm bg-violet-500 text-white text-2xl select-nonecursor-pointer">{props.title}!</div>
+        <div className="w-fit px-4 py-2 rounded-sm bg-violet-500 text-white text-2xl select-none cursor-pointer">{title}!</div>
         <ul>
-            {props.items.map(item => <li>Number {item}</li>)}
+            {items.map(item => <ListNumber num={item} />)}
         </ul>
         </>
     );
--- dopami/Controllers/WeatherForecastController.cs
+++ dopami/Controllers/WeatherForecastController.cs
@@ -3,14 +3,11 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
-namespace dopami.Controllers
-{
+namespace dopami.Controllers {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
-    {
+    public class WeatherForecastController : ControllerBase {
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
@@ -18,22 +15,18 @@ namespace dopami.Controllers
 
         private readonly ILogger<WeatherForecastController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
+        public WeatherForecastController(ILogger<WeatherForecastController> logger) {
             _logger = logger;
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
-        {
+        public IEnumerable<WeatherForecast> Get() {
             var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
                 Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+            }).ToArray();
         }
     }
 }
