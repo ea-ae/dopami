c17cb935206e73185562e0b3c45b3b55aa1c2952
ae <90932525+ea-ae@users.noreply.github.com>
2022-01-05 19:48:29 +0200

    Demo API endpoint

--- dopami/Controllers/IndexController.cs
+++ dopami/Controllers/IndexController.cs
@@ -3,10 +3,15 @@ using System.Text.Encodings.Web;
 
 namespace dopami.Controllers {
     public class IndexController : Controller {
-        [Route("app/")]
-        [Route("app/index")]
+        [Route("api/")]
+        [Route("api/index")]
         public string Index(string text = "default") {
             return HtmlEncoder.Default.Encode($"API Index, text={text}");
+        }
+        
+        [Route("api/print/{text=default}")]
+        public string Print(string text) {
+            return HtmlEncoder.Default.Encode($"print:{text}");
         }
     }
 }
