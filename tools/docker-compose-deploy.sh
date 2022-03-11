9bd8e6e06ea9f987dd563865b3c0080eb7afb900
ae <90932525+ea-ae@users.noreply.github.com>
2022-03-11 17:42:20 +0200

    Work on adding SSL support

--- tools/docker-compose-deploy.sh
+++ tools/docker-compose-deploy.sh
@@ -3,8 +3,8 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-set -a
-. $SCRIPT_DIR/../.env
-set +a
+# set -a
+# . $SCRIPT_DIR/../.env
+# set +a
 
-docker-compose --context one -f $SCRIPT_DIR/../docker-compose.yaml -f $SCRIPT_DIR/../docker-compose.prod.yaml up --build
+docker-compose --context one -f $SCRIPT_DIR/../docker-compose.yaml -f $SCRIPT_DIR/../docker-compose.prod.yaml up --build --detach
